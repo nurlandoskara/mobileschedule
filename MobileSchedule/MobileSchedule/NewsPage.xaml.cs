a67d2eb8766244f23f2262722635e3843b48a6f1
Doskarayev Nurlan <n.doskaraev@agrostream.net>
2018-11-03 16:12:36 +0600

    news

--- MobileSchedule/MobileSchedule/NewsPage.xaml.cs
+++ MobileSchedule/MobileSchedule/NewsPage.xaml.cs
@@ -1,20 +1,70 @@
-﻿using System;
+﻿using MobileSchedule.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace MobileSchedule
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class NewsPage : ContentPage
+    public partial class NewsPage : ContentPage
 	{
-		public NewsPage ()
+	    public ObservableCollection<News> News { get; set; }
+
+        public NewsPage ()
 		{
 			InitializeComponent ();
+		    ShowNews();
 		}
+
+	    private static async Task<dynamic> GetNewsAsync()
+	    {
+	        var queryString = "http://sschedule.azurewebsites.net/api/news";
+	        var news = await DataService.GetData(queryString).ConfigureAwait(false);
+	        return news;
+	    }
+
+	    private async void ShowNews()
+	    {
+	        try
+	        {
+	            var newsList = new List<News>();
+	            NewsList.IsRefreshing = true;
+	            var news = await GetNewsAsync();
+	            NewsList.EndRefresh();
+	            if (news != null)
+	            {
+	                foreach (var item in news)
+	                {
+	                    newsList.Add(new News() { Title = item.Title, Description = item.Description, Id = item.Id});
+	                }
+	            }
+	            News = new ObservableCollection<News>(newsList);
+	            NewsList.ItemsSource = News;
+	        }
+	        catch (Exception e)
+	        {
+	            await DisplayAlert("Серверге қосылу мүмкін емес", "Интернет қосылуын тексеріңіз", "OK");
+	            return;
+	        }
+	    }
+
+	    private void NewsList_OnRefreshingList_OnRefreshing(object sender, EventArgs e)
+	    {
+	        ShowNews();
+	    }
+
+	    private void SearchNewsText_OnSearchButtonPressed(object sender, EventArgs e)
+	    {
+	        string text = ((SearchBar)sender).Text;
+	        NewsList.ItemsSource = News.Where(x => x.Title.ToLower().Contains(text.ToLower())).ToList();
+        }
+
+	    private void SearchNewsText_OnTextChanged(object sender, TextChangedEventArgs e)
+	    {
+	        string text = ((SearchBar)sender).Text;
+	        NewsList.ItemsSource = News.Where(x => x.Title.ToLower().Contains(text.ToLower())).ToList();
+        }
 	}
 }
---
+++ MobileSchedule/MobileSchedule/ViewModels/News.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileSchedule.ViewModels
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
